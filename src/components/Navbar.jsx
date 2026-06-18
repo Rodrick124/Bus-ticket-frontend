3cb29b5f527e74ea20a8c2c359156d0f7877d263
Rodrick124 <victoroscarito@gmail.com>
2026-06-18 06:09:50 -0700

    Refactor code structure for improved readability and maintainability

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react'
 import { Link, useLocation } from 'react-router-dom'
-import Logo from '../assets/Tran-logo.png'
+import Logo from '../assets/Logo.svg'
 import Button from './Button'
 import { ThemeContext } from '../context/ThemeContext'
 import { Sun, Moon } from 'lucide-react';
